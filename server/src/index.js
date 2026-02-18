138ff6e44b62ba7530a251acde8cb00e975d230f
Codeman-lex <iamalexis100@gmail.com>
2026-02-18 22:10:05 +0100

    Fix Express 5 wildcard: '*' -> '/{*splat}'

--- server/src/index.js
+++ server/src/index.js
@@ -46,7 +46,7 @@ app.use('/api/admin', require('./routes/admin'));
 // Serve React build in production
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../../client/dist')));
-    app.get('*', (req, res) => {
+    app.get('/{*splat}', (req, res) => {
         res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
     });
 }
